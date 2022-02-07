7d86786c6c53b2e98d27752bdea519527c9ce372
Shyam-Dadhaniya <97106864+Shyam-Dadhaniya@users.noreply.github.com>
2022-02-07 18:53:59 +0530

    Update JS file

--- assest/script/script.js
+++ assest/script/script.js
@@ -1,12 +1,7 @@
 /*Initial Step*/
 let operation = document.getElementById('operation');
 
-<<<<<<< HEAD
 
-//-------------------------------------------Formate Of Digit Start-------------------------------------
-
-/*Formate of Number or Digits : This step is for formate the input and put semi-colon between digits*/
-=======
 /**
  * 
  * @param {*} input 
@@ -24,7 +19,6 @@ function getCurrentValue() {
 * @param input
 * @returns
 */
->>>>>>> Version-1
 function numberFormate(input) {
     return input.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -32,14 +26,7 @@ function numberFormate(input) {
 
 //-------------------------------------------All Normal Function Start----------------------------------
 
-/**
-<<<<<<< HEAD
- *  Operation : This function get the number when we click on button.
- */
-function addNum(e) {
-    let char = e.getAttribute("data-value");
-    let currentValue = operation.innerText.replace(/,/g, "");
-=======
+
  * @function addNum
  * @description This function get the number when we click on button.
  * @param {*} e 
@@ -47,7 +34,6 @@ function addNum(e) {
 function addNum(e) {
     let char = e.getAttribute("data-value");
     let currentValue = getCurrentValue();
->>>>>>> Version-1
     currentValue += char;
     operation.innerHTML = numberFormate(currentValue);
 }
@@ -55,15 +41,10 @@ function addNum(e) {
 
 
 //-------------------------------------------Clear Value Start------------------------------------------
-<<<<<<< HEAD
 
-/**
- * Clear Value : By Using This We can Clear All the Data
-=======
 /**
  * @function clearData
  * @description By Using This We can Clear All the Data
->>>>>>> Version-1
  */
 function clearData() {
     operation.innerText = "";
@@ -72,42 +53,26 @@ function clearData() {
 
 
 //-------------------------------------------Equal Start------------------------------------------------
-<<<<<<< HEAD
 
-/**
- * Equal : By using below function we will get the final result of the operation.
- */
-function cal() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function clearData
  * @description By using below function we will get the final result of the operation.
  */
 function cal() {
     let value = getCurrentValue();
->>>>>>> Version-1
     operation.innerText = numberFormate(eval(value).toString());
 }
 //-------------------------------------------Equal End--------------------------------------------------
 
 
 //-------------------------------------------Backspace Start--------------------------------------------
-<<<<<<< HEAD
 
-/**
- * BackSpace : By using this function we can erase last digit or method of opration.
- */
-function backspace() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function backspace
  * @description By using this function we can erase last digit or method of opration.
  */
 function backspace() {
     let value = getCurrentValue();
->>>>>>> Version-1
     let valuelength = value.length;
     let newValue = value.substring(0, valuelength - 1);
     operation.innerText = numberFormate(newValue);
@@ -116,21 +81,13 @@ function backspace() {
 
 
 //-------------------------------------------Plus-Minus Start-------------------------------------------
-<<<<<<< HEAD
 
-/**
- * Plus and Minus : By using this function we can change the value of the digits.(EX:We make sum of two digit and the final result of the operation is negative then by using this we can make that value plus)
-*/
-function plusminus() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function plusminus()
  * @description By using this function we can change the value of the digits.(EX:We make sum of two digit and the final result of the operation is negative then by using this we can make that value plus)
  */
 function plusminus() {
     let value = getCurrentValue();
->>>>>>> Version-1
     value *= -1;
     operation.innerText = numberFormate(value.toString());
 }
@@ -138,115 +95,71 @@ function plusminus() {
 
 
 //-------------------------------------------ln Start---------------------------------------------------
-<<<<<<< HEAD
 
-/**
- * ln : Here ln stands for log Natural and this function help as to find the log of any digits.
- */
-function ln() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function ln()
  * @description Here ln stands for log Natural and this function help as to find the log of any digits.
  */
 function ln() {
     let value = getCurrentValue();
->>>>>>> Version-1
     operation.innerText = Math.log(value).toString()
 }
 //-------------------------------------------ln End---------------------------------------------------
 
 
 //-------------------------------------------Log Start--------------------------------------------------
-<<<<<<< HEAD
 
-/**
- * log: Log generally refers to a logarithm to the base 10 this function help us to found log with base 10.
- */
-function log() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function log()
  * @description Log generally refers to a logarithm to the base 10 this function help us to found log with base.
  */
 function log() {
     let value = getCurrentValue();
->>>>>>> Version-1
     operation.innerText = Math.log10(value).toString();
 }
 //-------------------------------------------Log End----------------------------------------------------
 
 
 //-------------------------------------------Power of Ten Start-----------------------------------------
-<<<<<<< HEAD
 
-/**
- * 10^x : It's means power of ten and this function is help us to calculate the power of 10 and here x means whatever power we want.
- */
-function tenPower() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function tenPower()
  * @description 10^x : It's means power of ten and this function is help us to calculate the power of 10 and here x means whatever power we want.
  */
 function tenPower() {
     let value = getCurrentValue();
->>>>>>> Version-1
     operation.innerText = Math.pow(10, value).toString();
 }
 //-------------------------------------------Power of Ten End-------------------------------------------
 
 
 //-------------------------------------------Root Start-------------------------------------------------
-<<<<<<< HEAD
 
-/**
- * Root : It's help to find the square root of any value. 
- */
-function root() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function root()
  * @description It's help to find the square root of any value.
  */
 function root() {
     let value = getCurrentValue();
->>>>>>> Version-1
     operation.innerText = Math.sqrt(value).toString();
 }
 //-------------------------------------------Root End---------------------------------------------------
 
 
 //-------------------------------------------Factorial Start--------------------------------------------
-<<<<<<< HEAD
 
-/**
-* Factorial : Factorial is a function that multiplies a number by every number below it.(EX: 5!= 5*4*3*2*1=120.)
-*/
-
-function factorial() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function factorial()
  * @description Factorial is a function that multiplies a number by every number below it.(EX: 5!= 5*4*3*2*1=120.)
  */
 function factorial() {
     let value = getCurrentValue();
->>>>>>> Version-1
     let result = 1;
     let i = 0;
     for (let i = value; i >= 1; i--) {
         result *= i;
     }
-<<<<<<< HEAD
-=======
     // result = Math.factorial(value);
->>>>>>> Version-1
     operation.innerText = result.toString();
 }
 //-------------------------------------------Factorial End----------------------------------------------
@@ -254,21 +167,12 @@ function factorial() {
 
 
 //-------------------------------------------Square Start-----------------------------------------------
-<<<<<<< HEAD
-
-/**
- * Square : By using this function we can find the square of any numbers.
- */
-function square() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function square()
  * @description By using this function we can find the square of any numbers.
  */
 function square() {
     let value = getCurrentValue();
->>>>>>> Version-1
     value *= value;
     operation.innerText = value.toString();
 }
@@ -276,63 +180,36 @@ function square() {
 
 
 //-------------------------------------------Exp Start--------------------------------------------------
-<<<<<<< HEAD
-
-/**
- * Exp : This function is use to returns e^x value 
- */
-function cal_exp() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function calExp()
  * @description This function is use to returns e^x value 
  */
 function calExp() {
     let value = getCurrentValue();
->>>>>>> Version-1
     operation.innerText = Math.exp(value);
 }
 //-------------------------------------------Exp End----------------------------------------------------
 
 
 //-------------------------------------------Absolute Start---------------------------------------------
-<<<<<<< HEAD
-
-/**
- * Absolute : This function is use the make any value absolute.
- */
-function absolute() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function absolute()
  * @description This function is use the make any value absolute.
  */
 function absolute() {
     let value = getCurrentValue();
->>>>>>> Version-1
     operation.innerText = Math.abs(value);
 }
 //-------------------------------------------Absolute End-----------------------------------------------
 
 
 //-------------------------------------------Function Start---------------------------------------------
-<<<<<<< HEAD
-
-/**
- *  Math Function : By using this function we can make any value round, floor, ceil, trunc.
- */
-function mathsFun(fname) {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function mathsFunction()
  * @description By using this function we can make any value round, floor, ceil, trunc.
  */
 function mathsFunction(fname) {
     let value = getCurrentValue();
->>>>>>> Version-1
     let result = 0;
     switch (fname) {
         case 'ceil':
@@ -355,21 +232,12 @@ function mathsFunction(fname) {
 
 
 //-------------------------------------------Trigonometry Start-----------------------------------------
-<<<<<<< HEAD
-
-/**
- * Trigonometry Function : By using this function we can find the trigo value of any digits.
- */
-function trigonometry(method) {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function trigonometry()
  * @description By using this function we can make any value round, floor, ceil, trunc.
  */
 function trigonometry(method) {
     let value = getCurrentValue();
->>>>>>> Version-1
     let result = 0;
     switch (method) {
         case "sin":
@@ -395,21 +263,13 @@ function trigonometry(method) {
 
 
 //-------------------------------------------Degree Start-----------------------------------------------
-<<<<<<< HEAD
 
-/**
- *  Degree : This function is use to measures of an angle.
- */
-function deg() {
-    let value = operation.innerText.replace(/,/g, "");
-=======
 /**
  * @function deg()
  * @description convert radian to degree.
  */
 function deg() {
     let value = getCurrentValue();
->>>>>>> Version-1
     value *= (180 / Math.PI)
     operation.innerText = numberFormate(value.toString())
 }
@@ -417,15 +277,10 @@ function deg() {
 
 
 //-------------------------------------------Exponential Start------------------------------------------
-<<<<<<< HEAD
 
-/**
- *  Exponential : This function is use to fixed value with exponent value.
-=======
 /**
  * @function exp()
  * @description This function is use to fixed value with exponent value.
->>>>>>> Version-1
  */
 function exp() {
     let value = Number(operation.innerText.replace(/,/g, ""));
@@ -437,18 +292,10 @@ function exp() {
 //-------------------------------------------Memory Function Start--------------------------------------
 
 let mValue = 0;
-<<<<<<< HEAD
-
-/**
- * Memory Store : The calculator has one memory that can be used for storing values temporarily.
- */
-
-=======
 /**
  * @function memoryStore()
  * @description The calculator has one memory that can be used for storing values temporarily.
  */
->>>>>>> Version-1
 function memoryStore() {
     let value = Number(operation.innerText.replace(/,/g, ""));
     if (operation.innerText != "") {
@@ -458,18 +305,11 @@ function memoryStore() {
     }
     operation.innerText = "";
 }
-
 /**
-<<<<<<< HEAD
- * Memory Plus : This function use to add the presently displayed number to the value in memory.
- */
-function mPlus() {
-=======
  * @function memoryPlus()
  * @description This function use to add the presently displayed number to the value in memory.
  */
 function memoryPlus() {
->>>>>>> Version-1
     let value = Number(operation.innerText.replace(/,/g, ""));
     if (operation.innerText != "") {
         mValue += value;
@@ -480,16 +320,10 @@ function memoryPlus() {
 }
 
 /**
-<<<<<<< HEAD
- * Memory Minus : This function use to subtract the present value from the stored value.
-*/
-function mMinus() {
-=======
  * @function memoryPlus()
  * @description This function use to subtract the present value from the stored value.
  */
 function memoryMinus() {
->>>>>>> Version-1
     let value = Number(operation.innerText.replace(/,/g, ""));
     if (operation.innerText != "") {
         mValue -= value;
@@ -499,38 +333,20 @@ function memoryMinus() {
     operation.innerText = "";
 }
 
-<<<<<<< HEAD
-/**
- * Memory Recall : This function is used to display the number saved in memory,The calculator will show the stored number on screen, replacing any value already displayed.
- */
-
-function mRecall() {
-=======
-
 /**
  * @function memoryPlus()
  * @description This function is used to display the number saved in memory,The calculator will show the stored number on screen, replacing any value already displayed.
  */
 function memoryRecall() {
->>>>>>> Version-1
     console.log(mValue)
     operation.innerText = numberFormate(mValue.toString());
 }
 
-<<<<<<< HEAD
-
-/**
- * Memory Clear: The calculator has one memory that can be used for storing values temporarily to clear this memory we need to use this function.
- */
-
-function mClear() {
-=======
 /**
  * @function memoryPlus()
  * @description The calculator has one memory that can be used for storing values temporarily to clear this memory we need to use this function.
  */
 function memoryClear() {
->>>>>>> Version-1
     mValue = 0
     if (mValue == 0)
         document.getElementById("mc-on").style.color = "gray";
